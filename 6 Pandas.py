b7a4ac8d5bf7a420329b6e9334ad4b2e7b9504ef
ValerianX <vyomesh.1907@GMAIL.COM>
2022-12-06 01:18:41 +0530

    next is groupby

--- 6 Pandas.py
+++ 6 Pandas.py
@@ -21,7 +21,7 @@ print(date_series)
 print(type(date_series))
 
 #############################################
-# Indexing aand slicing - similar to Numpy arrays
+# Indexing and slicing - similar to Numpy arrays
 
 print(array1[1])              # Strictly positional
 print(array1[:-2])
@@ -33,9 +33,8 @@ print(pd.Series(np.array(range(10))**2, index=range(0, 10)))        # Explicitly
 #####################################################################################################
 # DataFrames
 
-df = pd.DataFrame({'name': ['Vinay', 'Kushal', 'Aman', 'Saif'],
-                   'age': [22, 25, 24, 28],
-                    'occupation': ['engineer', 'doctor', 'data analyst', 'teacher']})
+df = pd.DataFrame({'name': ['Vinay', 'Kushal', 'Aman', 'Saif'], 'age': [22, 25, 24, 28], 'occupation': ['engineer', 'doctor',
+                                                                                                        'data analyst', 'teacher']})
 print(df)
 print("\n")
 
@@ -92,9 +91,50 @@ print(market_df.iloc[2, 4])             # axis 0 = 2nd element and axis 1 = 5th 
 print(market_df.iloc[3])                # By default, axis 0 is given 1st position and assigned value 2 and axis 1 all selected
 print(market_df.iloc[2, :3])            # 2nd row but only up to 3rd column
 print(market_df.iloc[[1, 3, 7], 1:3])   # Pass list of selected/indexed row positions and selected/sliced columns
-
+print('\n')
 # Label Based Indexing
 
+""" It is dissimilar to Position only in the sense that the only arguments it accepts are actual labels - default labels for rows are 
+numbers starting from 0 but if the index is changed to a column name then that key has to be passed in arguments"""
 
+print(market_df.loc[2, 'Sales'])
+market_df.set_index('Ord_id', inplace=True)
+print(market_df)
 
+print(market_df.loc[['Ord_5406', 'Ord_5446', 'Ord_5485'], 'Sales':'Profit'])    # specific columns can be passed as a list
 
+print(market_df.Sales > 3000)                  # Gives True and False against each row and COLUMN NAMES ARE CASE SENSITIVE
+print(market_df.loc[market_df.Sales > 3000])          # Gives only True values for the Label Sales on axis = 1
+
+print(market_df.loc[(market_df['Sales'] > 3000) & (market_df['Profit'] < 500), :])
+print('\n')
+print(market_df.loc[(market_df.Sales > 2000) | (market_df.Discount == 0.01)], ['Cust_id', 'Sales', 'Profit'])
+print('\n')
+customers_in_bangalore = ['Cust_1798', 'Cust_1519', 'Cust_637', 'Cust_851']
+print(market_df.loc[market_df['Cust_id'].isin(customers_in_bangalore), :])
+
+"""Merge/Join"""
+
+customer_df = pd.read_csv(r"C:\Users\vyome\OneDrive\Desktop\PGDMLAI\0 Pre preparatory\Python for Data Science\2_Introduction to "
+                          r"Pandas\global_sales_data\cust_dimen.csv")
+df1 = pd.merge(market_df, customer_df, how='inner', on='Cust_id')
+print(df1)
+# Merge or Join can only be applied to 2 dataframes at a time, how=inner/left/right/outer
+
+"""Concatenation or Append or UNION"""
+print(pd.concat([market_df, customer_df], axis=0))      # appends at the bottom if the columns are same
+print('\n')
+
+"""
+Arithmetic operations that can be applied to dataframes:
+add(): +
+sub(): -
+mul(): *
+div(): /
+floordiv(): //
+mod(): %
+pow(): **
+"""
+
+# Total = IPL_2018 + IPL_2017     # Applying on 2 DF with same columns but this throws a lot of NaN values in case of non-matching values
+# Total = IPL_2018.add(IPL_2017, fill_value = 0)      # Alternatively to fill zeros for NaN values
